6e70c737eb3e7424e92098d6ecfed58c6d409c4f
Audrey <audrey_cooper@me.com>
2021-01-07 17:49:46 -0800

    added routes to welcome page and access to react

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,6 @@ class PagesController < ApplicationController
   # root path for react application
   def index 
   end
+  def welcome
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'pages/welcome'
+  get '/index', to: 'pages#index', as: 'index'  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'pages#index'
+  root to: 'pages#welcome'
+  # root to: 'pages#index'
 end
