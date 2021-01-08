488c13f1ef68850a80b7821198d0389f1f32df64
Audrey <audrey_cooper@me.com>
2021-01-08 11:50:07 -0800

    devise user model created, routes added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
-  get 'pages/welcome'
-  get '/index', to: 'pages#index', as: 'index'  
+  devise_for :users
+  authenticated :user do
+    root "pages#index", as: :authenticated_root
+  end
+   
+  get 'pages/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#welcome'
   # root to: 'pages#index'
