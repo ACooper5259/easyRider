f84ad65731a58cd6211b8f8d9cdfb23ca9405066
Audrey <audrey_cooper@me.com>
2021-01-11 17:11:50 -0800

    add profile model, controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,11 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'pages#welcome'
   # root to: 'pages#index'
+  
+  namespace :api do
+    namespace :v1 do
+      resources :bookings
+    end
+  end
+
 end
